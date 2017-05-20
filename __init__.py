d987ca0c1fff60e01e5bd6f2b786327ada2648a0
victor-iyiola <javafolabi@gmail.com>
2017-05-20 02:40:42 +0100

    Modified code for more readability

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from controller import *
+from views import *
 
 if __name__ == '__main__':
     app.run(debug=True)
--- views.py
+++ views.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, \
-     request, flash, abort, session
+from flask import Flask, render_template, redirect, url_for, request, flash, abort, session
 import os, gc
 
 from helpers import config
